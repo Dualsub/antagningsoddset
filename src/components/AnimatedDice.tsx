5a33e9596ba4f21411e27ab25d2c4c79435b9922
Dualsub <rizedualsub@gmail.com>
2023-06-24 12:41:21 +0200

    Updates

--- src/components/AnimatedDice.tsx
+++
@@ -1,9 +0,0 @@
-import "../index.css";
-
-interface AnimatedDiceProps {
-    className?: string
-};
-
-export const AnimatedDice = ({ className }: AnimatedDiceProps) => <>
-    <h1 className={"dice " + className || ""}>🎲</h1>
-</>;
